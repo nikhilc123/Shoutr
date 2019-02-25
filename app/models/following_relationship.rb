672d9b55479a87a65516d700a49e4f767359494e
Nikhil Chikorde <nikhilc.developer@gmail.com>
2019-02-25 15:24:35 -0500

    Count the followers and self_followed_users

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,6 +1,8 @@
 class FollowingRelationship < ApplicationRecord
   #follower because - find all the followed_users
   # follower_id is nothing but user id
-  belongs_to :follower, class_name: 'User'
-  belongs_to :followed_user, class_name: 'User'
+  # a user follows b.. increase the count of b
+  # and then increase the count of a
+  belongs_to :follower, class_name: 'User', counter_cache: :followed_users_count
+  belongs_to :followed_user, class_name: 'User', counter_cache: :followers_count
 end
